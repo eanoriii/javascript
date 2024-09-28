93bc777b69307c42f11db48cf051db43bca3e5be
Marcella Harumy <marcella.harumy@gmail.com>
2024-09-28 11:55:03 -0300

    Add files via upload

--- projeto_28_09_24/script.js
+++ projeto_28_09_24/script.js
@@ -2,7 +2,7 @@ function clickar(){
     let nome = prompt ("qual é seu nome?");
     let idade = prompt ("qul é sua idade?");
     let cidade = prompt ("qual é sua cidade?");
-    alert  ("olá" + nome + "você é de" + cidade + " e tem" + idade + " anos");
+    alert  ("olá, " + nome + "! você é de " + cidade + " e tem " + idade + " anos");
     console.log("nome de usuario:", nome);
     console.log("idade:", idade);
     console.log("cidade:", cidade);
